f407b010e77979085c48f3fa4fd210a91e8e60da
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-09-23 18:18:30 -0300

    formating text

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -3,21 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: 'Frases do dia',
-      home: Column(
-        children: <Widget>[
-          Text('Oi amor'),
-          Text('Oi amor'),
-          Text('Oi amor'),
-          Row(
-            children: [
-              Text('Oi'),
-              Text('Oi'),
+        title: 'Frases do dia',
+        home: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              Text(
+                  'Lorem Ipsum is simply dumm SDADA Sad asdas dsd kjash dksdjaskjd skjah jk hkj hkj hkj hkj hkj hkjhk',
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300,
+                    decoration: TextDecoration.none
+                  ))
             ],
-          )
-        ],
-      ),
-      // home: Container(color: Colors.white)
-    ),
+          ),
+        )),
   );
 }
