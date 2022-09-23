00a692a85b2e5e573c79654f3eccd9e9deb4f446
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-09-23 16:49:53 -0300

    working with column, row and text

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp( 
-    title: 'Frases do dia',
-    home: Container(color: Colors.white,)
-  ));
+  runApp(
+    MaterialApp(
+      title: 'Frases do dia',
+      home: Column(
+        children: <Widget>[
+          Text('Oi amor'),
+          Text('Oi amor'),
+          Text('Oi amor'),
+          Row(
+            children: [
+              Text('Oi'),
+              Text('Oi'),
+            ],
+          )
+        ],
+      ),
+      // home: Container(color: Colors.white)
+    ),
+  );
 }
