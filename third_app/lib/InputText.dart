a32ebc585e699ea5e73eb5437cf73fd531a548cc
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-09-25 16:35:16 -0300

    fixing onChange state campo

--- third_app/lib/InputText.dart
+++ third_app/lib/InputText.dart
@@ -9,42 +9,41 @@ class MyTextInput extends StatefulWidget {
 }
 
 class _MyTextInputState extends State<MyTextInput> {
-
   final _textEditingController = TextEditingController();
   String campo = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-     appBar: AppBar(
-      title: Text('Entrada')
-     ),
-     body: Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(32),
-          child: TextField(
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              labelText: 'Digite um valor',
-            ),
-            maxLength: 2,
-            maxLengthEnforcement: MaxLengthEnforcement.none,
-            onSubmitted: (String texto ) => {
-              campo = texto,
-            },
-            controller: _textEditingController,
-            obscureText: true,
-          )
-        ),
-        ElevatedButton(onPressed: (){
-          print(_textEditingController.text);
-        }, child: Text("Clique")),
-        Text(campo),
-        Text(_textEditingController.text),
-      ],
-     ), 
+      appBar: AppBar(title: Text('Entrada')),
+      body: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(32),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Digite um valor',
+                ),
+                maxLength: 2,
+                maxLengthEnforcement: MaxLengthEnforcement.none,
+                onChanged: (String texto) => {
+                  setState(() {
+                    campo = texto;
+                  })
+                },
+                controller: _textEditingController,
+                obscureText: true,
+              )),
+          ElevatedButton(
+              onPressed: () {
+                print(_textEditingController.text);
+              },
+              child: Text("Printar")),
+          Text(campo),
+          Text(_textEditingController.text),
+        ],
+      ),
     );
   }
 }
