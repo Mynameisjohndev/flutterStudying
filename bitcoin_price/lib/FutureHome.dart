c16dd31308326665a2f47ec2562f91cf8cd431ed
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-09-28 15:14:55 -0300

    Making post post"

--- bitcoin_price/lib/FutureHome.dart
+++ bitcoin_price/lib/FutureHome.dart
@@ -11,65 +11,92 @@ class FutureHome extends StatefulWidget {
 }
 
 class _FutureHomeState extends State<FutureHome> {
-  
   String _urlBase = "https://jsonplaceholder.typicode.com";
- 
-  Future<List<Post>> _requestPosts() async{
-    Api.Response response = await Api.get( Uri.parse(_urlBase + "/posts") );
-    var responseDecoded = json.decode( response.body );
-    List<Post> posts = [];
 
-    for (var post in responseDecoded){
-      Post newPost = Post(
-        post["userId"], 
-        post["id"], 
-        post["title"], 
-        post["body"]
-      );
+  Future<List<Post>> _requestPosts() async {
+    Api.Response response = await Api.get(Uri.parse(_urlBase + "/posts"));
+    var responseDecoded = json.decode(response.body);
+    List<Post> posts = [];
+    for (var post in responseDecoded) {
+      Post newPost =
+          Post(post["userId"], post["id"], post["title"], post["body"]);
       posts.add(newPost);
     }
-
     return posts;
+  }
+
+  _post() async{
+
+    Post post = new Post(120, 1, "Titulo", "Corpo da postagem");
+
+    var corpo = json.encode(
+        post.toJson()
+    );
+    
+    Api.Response response = await Api.post(
+      Uri.parse(_urlBase + "/posts"),
+      body: corpo,
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }, 
+    );
+
+    print(response.statusCode);
+    print(response.body);
+
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    
     return Scaffold(
       appBar: AppBar(
         title: Text("Consumo de serviço"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _requestPosts(),
-        builder: (context, snapshot){
-        var result;
-        switch(snapshot.connectionState){
-            case ConnectionState.waiting: 
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            case ConnectionState.done:
-              if(snapshot.hasError){
-                result = "Erro ao carregar os dados";
-              }else{
-                print("Carregou");
-                result = ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  List<Post>? lista = snapshot.data;
-                  Post post = lista![index];
-                  return ListTile(
-                    title: Text(post.title),
-                    subtitle: Text(post.id.toString()),
-                  );
-                });
-              }
-              break;
-          }
-          return result;
-        },
-      ),
+      body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                ElevatedButton(onPressed: _post, child: Text("Salvar")),
+                ElevatedButton(onPressed: null, child: Text("Atualizar")),
+                ElevatedButton(onPressed: null, child: Text("Deletar")),
+              ]),
+              Expanded(
+                child: FutureBuilder<List<Post>>(
+                  future: _requestPosts(),
+                  builder: (context, snapshot) {
+                    var result;
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      case ConnectionState.done:
+                        if (snapshot.hasError) {
+                          result = "Erro ao carregar os dados";
+                        } else {
+                          print("Carregou");
+                          result = ListView.builder(
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (context, index) {
+                                List<Post>? lista = snapshot.data;
+                                Post post = lista![index];
+                                return ListTile(
+                                  title: Text(post.title),
+                                  subtitle: Text(post.id.toString()),
+                                );
+                              });
+                        }
+                        break;
+                    }
+                    return result;
+                  },
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
--- bitcoin_price/lib/Post.dart
+++ bitcoin_price/lib/Post.dart
@@ -1,35 +1,44 @@
 class Post{
 
-  late int _userId;
-
-  late int _id;
-  late String _title;
-  late String _body;
+  int _userId;
+  int _id;
+  String _title;
+  String _body;
 
   Post(this._userId, this._id, this._title, this._body);
 
-  int get userId => _userId;
-
-  set userId(int userId) {
-    _userId = userId;
+  Map toJson(){
+    return {
+      "userId": this._userId,
+      "id": this._id,
+      "title": this._title,
+      "body": this._body
+    };
   }
 
-  int get id => _id;
+  String get body => _body;
 
-  set id(int _id) {
-    _id = id;
+  set body(String value) {
+    _body = value;
   }
 
   String get title => _title;
 
-  set title(String _title) {
-    _title = title;
+  set title(String value) {
+    _title = value;
   }
-  
-  String get body => _body;
 
-  set body(String _body) {
-    _body = body;
+  int get id => _id;
+
+  set id(int value) {
+    _id = value;
   }
+
+  int get userId => _userId;
+
+  set userId(int value) {
+    _userId = value;
+  }
+
 
 }
