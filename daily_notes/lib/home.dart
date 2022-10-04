eb189724f5cee8cf3eb7601be0fdb8621fc0edca
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-10-04 09:43:58 -0300

    Creating interface daily notes

---
+++ daily_notes/lib/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+
+  _exibirTelaCadastro() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Adicionar anotação"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  controller: _titleController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "Título", hintText: "Digite título..."),
+                ),
+                TextField(
+                  controller: _descriptionController,
+                  decoration: InputDecoration(
+                      labelText: "Descrição", hintText: "Digite descrição..."),
+                )
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancelar")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Salvar"))
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas anotações"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          child: Icon(Icons.add),
+          onPressed: () {
+            _exibirTelaCadastro();
+          }),
+    );
+  }
+}
