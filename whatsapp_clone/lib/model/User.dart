1b9b3db97a19575f9cb5ed8ae8431df9d4da7b64
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-10-19 11:16:49 -0300

    Listing contacts

--- whatsapp_clone/lib/model/User.dart
+++ whatsapp_clone/lib/model/User.dart
@@ -1,34 +1,43 @@
 class MyUser{
   String? _name;
   String? _email;
+  String? _profile;
   String? _password;
-
+ 
   MyUser();
 
   Map<String, dynamic> toMap(){
+
     Map<String, dynamic> map = {
-      "name": this.name,
-      "email": this.email,
+      "name" : this.name,
+      "email" : this.email
     };
+
     return map;
+
   }
 
-  String? get name => _name;
+  String? get profile => _profile;
 
-  set name(String? name) {
-    _name = name;
-  }
-
-  String? get email => _email;
-
-  set email(String? email) {
-    _email = email;
+  set profile(String? value) {
+    _profile = value;
   }
 
   String? get password => _password;
 
-  set password(String? password) {
-    _password = password;
+  set password(String? value) {
+    _password = value;
   }
 
+  String? get email => _email;
+
+  set email(String? value) {
+    _email = value;
+  }
+
+  String? get name => _name;
+
+  set name(String? value) {
+    _name = value;
+  }
 }
--- whatsapp_clone/lib/screens/contacts.dart
+++ whatsapp_clone/lib/screens/contacts.dart
@@ -1,42 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/model/Chat.dart';
+import 'package:whatsapp_clone/model/User.dart';
 
 class Contacts extends StatefulWidget {
   const Contacts({super.key});
 
   @override
   State<Contacts> createState() => _ContactsState();
-} 
+}
 
 class _ContactsState extends State<Contacts> {
 
-  List<Chat> chatList = [
-    Chat("Meu amor ❤", "Como você vai", "https://firebasestorage.googleapis.com/v0/b/whatsappflutter-715ad.appspot.com/o/userPhoto%2F297198350_744858636953138_6294962469889980768_n.jpg?alt=media&token=58a0ebb0-d90d-4dff-84ff-d08a7329cddb"),
-    Chat("Meu amor ❤", "Como você vai", "https://firebasestorage.googleapis.com/v0/b/whatsappflutter-715ad.appspot.com/o/userPhoto%2F297198350_744858636953138_6294962469889980768_n.jpg?alt=media&token=58a0ebb0-d90d-4dff-84ff-d08a7329cddb"),
-    Chat("Meu amor ❤", "Como você vai", "https://firebasestorage.googleapis.com/v0/b/whatsappflutter-715ad.appspot.com/o/userPhoto%2F297198350_744858636953138_6294962469889980768_n.jpg?alt=media&token=58a0ebb0-d90d-4dff-84ff-d08a7329cddb"),
-  ];
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String? idUser;
+  String? emailUser;
+
+  _loadUserData() async {
+    var logedUser = FirebaseAuth.instance.currentUser;
+    idUser = logedUser!.uid;
+    emailUser = logedUser.email;
+  }
+
+  Future<List<MyUser>> _loadContacts() async {
+    QuerySnapshot response = await db.collection('users').get();
+    List<MyUser> users = [];
+    for (DocumentSnapshot item in response.docs) {
+      var dados = item.data() as Map;
+      if(dados["email"] == emailUser) continue;
+      MyUser usuario = MyUser();
+      usuario.email = dados["email"];
+      usuario.name = dados["name"];
+      usuario.profile = dados["profile"];
+      users.add(usuario);
+    }
+    return users;
+  }
+
+  @override
+  void initState() {
+    _loadUserData();
+    _loadContacts();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: chatList.length,
-      itemBuilder: (BuildContext context, int index) { 
-        Chat chat = chatList[index];
-        return ListTile(
-          contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-          leading: CircleAvatar(
-            maxRadius: 30,
-            backgroundColor: Colors.grey,
-            backgroundImage: NetworkImage(chat.pathPhoto!),
-          ),
-          title: Text(
-            chat.name!,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
-          ),
-        );
+    return FutureBuilder<List<MyUser>>(
+      future: _loadContacts(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text("Carregando contatos"),
+                  CircularProgressIndicator()
+                ],
+              ),
+            );
+          case ConnectionState.active:
+          case ConnectionState.done:
+            return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (_, index) {
+                  List<MyUser>? listaItens = snapshot.data;
+                  MyUser usuario = listaItens![index];
+                  return ListTile(
+                    contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                    leading: CircleAvatar(
+                      maxRadius: 30,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: usuario.profile != null
+                          ? NetworkImage(usuario.profile!)
+                          : null,
+                    ),
+                    title: Text(
+                      usuario.name!,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  );
+                });
+            break;
+        }
+        return Container();
       },
     );
   }
