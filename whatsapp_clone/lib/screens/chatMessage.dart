5e03cb2823aa7474fef69f882779b09f8102b1bd
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-10-20 14:22:08 -0300

    Creating textFieldMessage component

--- whatsapp_clone/lib/screens/chatMessage.dart
+++ whatsapp_clone/lib/screens/chatMessage.dart
@@ -10,17 +10,85 @@ class ChatMessage extends StatefulWidget {
 
 class _ChatMessageState extends State<ChatMessage> {
   
+  final TextEditingController _controllerMensagem = TextEditingController();
+
+  _enviarMensagem(){
+
+  }
+
+  _enviarFoto(){
+
+  }
+
+  
+
+
   @override
   Widget build(BuildContext context) {
+
+    var messsageTextField = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                controller: _controllerMensagem,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 16),
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(0, 8, 32, 8),
+                    hintText: "Digite uma mensagem...",
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Color(0xff075E54)),
+                  borderRadius: BorderRadius.circular(32)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)
+                    ),
+                    prefixIcon: IconButton(
+                        icon: Icon(Icons.camera_alt, color: Color(0xff075E54)),
+                        onPressed: _enviarFoto
+                    )
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xff075E54),
+            child: Icon(Icons.send, color: Colors.white, size: 20),
+            mini: true,
+            onPressed: _enviarMensagem,
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.user == null ? "" : widget.user!.name!),
         backgroundColor: Color(0xff075e54),
       ),
       body: Container(
-        // decoration: ,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bg.png"),
+            fit: BoxFit.cover
+          )
+        ),
         child: SafeArea(
-          child: Container()
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: Column(  
+              children: <Widget>[
+                messsageTextField
+              ],
+            ),
+          )
         ),
       ),
     );
