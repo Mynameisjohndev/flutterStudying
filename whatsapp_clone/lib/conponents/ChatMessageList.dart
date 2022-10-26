84a2d4a98baa911ea99769f6a22893d84a6b0f88
Mynameisjohndev <joaoantonio15161516@gmail.com>
2022-10-25 22:54:33 -0300

    Showing hour message

--- whatsapp_clone/lib/conponents/ChatMessageList.dart
+++ whatsapp_clone/lib/conponents/ChatMessageList.dart
@@ -15,28 +15,45 @@ class _ChatMessageListState extends State<ChatMessageList> {
           itemCount: widget.messages.length,
           itemBuilder: (context, index) {
             var item = widget.messages[index];
-            double containerWidth = MediaQuery.of(context).size.width* 0.7;
+            double containerWidth = MediaQuery.of(context).size.width * 0.7;
             Color mycolor = Color(0xffd2ffa5);
             Alignment myAlignment = Alignment.centerRight;
+            Alignment hourMessage = Alignment.centerLeft;
+            CrossAxisAlignment crossAxisAlignmentMessage = CrossAxisAlignment.end;
             if (index % 2 == 0) {
               mycolor = Colors.white;
               myAlignment = Alignment.centerLeft;
+              hourMessage = Alignment.centerRight;
+              crossAxisAlignmentMessage = CrossAxisAlignment.start;
             }
             return Align(
               alignment: myAlignment,
               child: Padding(
                 padding: EdgeInsets.all(6),
                 child: Container(
-                  width: containerWidth,
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                      color: mycolor,
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  child: Text(
-                    item,
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
+                    width: containerWidth,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                        color: mycolor,
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: crossAxisAlignmentMessage,
+                      children: [
+                        Text(
+                          item,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Align(
+                          alignment: hourMessage,
+                          child: Text(
+                            "20:08",
+                            style: TextStyle(
+                                fontSize: 14, color: Colors.grey[400]),
+                          ),
+                        )
+                      ],
+                    )),
               ),
             );
           }),
